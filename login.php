2bfc534a298ed14074665c04218202e4a6504fb1
Isabella <2197099@talnet.nl>
2025-07-01 11:21:43 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,41 +1,59 @@
-<!DOCTYPE php>
-<html lang="en">
+<?php
+require('config.php');
+session_start();
 
+
+if (isset($_POST["submit"])) {
+    $emaillogin = $_POST["email"];
+    $pwlogin = $_POST["password"];
+
+    
+    $stmt = $conn->prepare("SELECT * FROM tb_user WHERE email = ?");
+    $stmt->bind_param("s", $emaillogin);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if (mysqli_num_rows($result) > 0) {
+   $row = mysqli_fetch_assoc($result);
+   
+ 
+   if (password_verify($pwlogin, $row["password"])) {
+   $_SESSION["login"] = true;
+   $_SESSION["id"] = $row["id"];
+   $_SESSION["email"] = $row["email"];
+    header("Location: users.php");
+    exit;
+    } else {
+    echo "<script>alert('Wrong password');</script>";
+    }
+    } else {
+    echo "<script>alert('User is not registeered');</script>";
+
+}
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Forum</title>
-  <link rel="stylesheet" href="css/style.css">
+  <title>Login</title>
+  <link rel="stylesheet" href="loginn.css">
 </head>
-
 <body>
 
-<div class="button container">
- <div class="login container">
-<form class="box" style="color: rgb(148, 5, 5);"> 
 <h2>Login</h2>
-<label for="username">Username</label>
-<input type="text" id="username" name="username" placeholder="Enter your username">
-<label for="password">Password</label>
-<input type="password" id="password" name="password" placeholder="Enter your password">
-<div class="button-container">
-<button type="submit">Login</button>
-</div>
-</div>
-</form>
-</div>
 
-<form action="welcome.php" method="POST">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
+<form action="login.php" method="POST" class="box" style="color: rgb(148, 5, 5);">
+  Email: <input type="text" name="email" required> <br>
+  Password: <input type="password" name="password" required><br>
+  <input type="submit" name="submit" value="Login">
 </form>
 
-  <?php
-  
-  
-  ?>
+<br>
+<a href="registration.php">Registration</a>
 
 </body>
-
 </html>

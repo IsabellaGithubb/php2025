a7c6420c594d439799cb61ac801de96a8cc3d6f4
Isabella <2197099@talnet.nl>
2025-07-01 11:21:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["login"]) || $_SESSION["login"] !== true) {
+header("Location: login.php");
+exit;
+}
+ require 'config.php';
+$id = $_SESSION["id"];
+
+$result = mysqli_query($conn, "SELECT * FROM tb_user WHERE id = $id");
+$row = mysqli_fetch_assoc($result);
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +21,7 @@
   <title>Index</title>
 </head>
 <body>
-  <h1>Welcome !!</h1>
+  <h1>Welcome <?php echo $row["name"];?></h1>
   <a href = "logout.php">Logout</a>
 </body>
 </html>

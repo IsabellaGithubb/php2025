0348fcf51b3379d80e3837459ea149ce55741ec0
Isabella <2197099@talnet.nl>
2025-07-01 11:22:43 +0200

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,12 +1,20 @@
-<?php 
-require 'config.php';
+<?php
+session_start();
+//$conn = require_once "partials/dbconnection.php";
+//var_dump($_POST);   
+
+
+
+require('config.php');
+
 if(isset($_POST["submit"])){
-$name $_POST["name"];
-$username $_POST["username"];
-$email $_POST["email"];
-$password $_POST["password"];
-$confirmpassword $_POST["confirmpassword"];
-$duplicate mysqli_query($conn "SELECT * FROM tb_user WHERE username = '$username' OR email = '$email'");
+
+$email = $_POST["email"];
+$password = $_POST["password"];
+$confirmpassword = $_POST["confirmpassword"];
+$duplicate = mysqli_query($conn, "SELECT * FROM tb_user WHERE email = '$email'");
+$stmt = $conn->prepare("SELECT * FROM tb_user WHERE email = ?");
+
 if(mysqli_num_rows($duplicate)> 0){
     echo
     "<script> alert('Username or email has alredy been taken'); </script>";
@@ -14,7 +22,8 @@ if(mysqli_num_rows($duplicate)> 0){
 }
 else{
     if ($password == $confirmpassword){
-        $query ="INSERT INTO tb_user VALUES('', '$name', '$username', '$email', '$password')";
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        $query = "INSERT INTO tb_user ( email, password) VALUES ('$email', '$password')";
         mysqli_query($conn,$query);
         echo
         "<script> alert('Registration successful'); </script>";
@@ -34,20 +43,17 @@ else{
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" dir="ltr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration</title>
+    <link rel="stylesheet" href="registration.css">
 </head>
 <body>
     <h2>Registration</h2>
-    <form class="" action="" method="post" autocomplete="off">
-        <label for="name">Name : </Label>
-        <input type="text" name="name" id = "name" required value =""> <br>
-       
-        <label for="username">Username : </Label>
-        <input type="text" name="username" id = "username" required value =""> <br>
+    <form class="" action="registration.php" method="post" autocomplete="off">
+
        
         <label for="email">Email : </Label>
         <input type="email" name="email" id = "email" required value =""> <br>
@@ -62,5 +68,7 @@ else{
 </form>
 <br>
 <a href="login.php">Login</a>
+
+
 </body>
 </html>
